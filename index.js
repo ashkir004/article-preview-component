0a80fa620d1d1589485bba4b57a8fc19a54927c9
ashkir <55173206+ashkir004@users.noreply.github.com>
2025-09-17 00:39:49 +0300

    finish solution

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let btnShareAction = document.getElementById('btn-share-action');
 let shareToast = document.getElementById('share-toast');
 
 btnShareAction.addEventListener('click', (e) => {
+    shareToast.toggleAttribute('aria-hidden');
     shareToast.classList.toggle('visible');
     btnShareAction.classList.toggle('share-dark');
 });
